d5bf68041395d8f4607abef728fe6fffe01a511b
Alex Culliere <nachasic@gmail.com>
2022-12-13 12:40:44 +0200

    Set default font
    add ability to change scale at runtime with neovide

--- init.lua
+++ init.lua
@@ -1,1 +1,20 @@
+vim.api.nvim_exec(
+[[
+if exists("g:neovide")
+  set guifont=DankMono\ Nerd\ Font\ Mono:h11
+
+  let  g:neovide_scale_factor=1.0
+  function! ChangeScaleFactor(delta)
+    let g:neovide_scale_factor = g:neovide_scale_factor * a:delta
+  endfunction
+
+  function! ResetScaleFactor()
+    let g:neovide_scale_factor=1.0
+  endfunction
+
+  nnoremap <expr><C-=> ChangeScaleFactor(1.05)
+  nnoremap <expr><C--> ChangeScaleFactor(1/1.05)
+  nnoremap <expr><C-0> ResetScaleFactor()
+endif
+]], true)
 require("alexculliere")
