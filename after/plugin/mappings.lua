1d66f2b6b3a3f734cba9cf5765463e17edfb49ff
Alex Culliere <nachasic@gmail.com>
2022-12-13 12:02:03 +0200

    Add Mason for DAP and LSP capabilities

--- after/plugin/mappings.lua
+++ after/plugin/mappings.lua
@@ -5,4 +5,19 @@ wk.register({
         name = "Execute",
         x = { "<cmd>:Ex<CR>", "File explorer" },
     },
+    l = {
+        name = "LSP",
+        e = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Diagnostics" },
+        k = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
+        s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
+        r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
+        g = {
+            name = "go to",
+            d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
+            D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
+            i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Implementation" },
+            r = { "<cmd>lua vim.lsp.buf.references()<CR>", "References" },
+        },
+
+    }
 }, { prefix = "<leader>" })
