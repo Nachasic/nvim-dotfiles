c11734c81abb57901dbb94da919e63bb3b45a1dc
Alex Culliere <nachasic@gmail.com>
2022-12-13 13:33:11 +0200

    Easy navigation between splits

--- init.lua
+++ init.lua
@@ -12,9 +12,16 @@ if exists("g:neovide")
     let g:neovide_scale_factor=1.0
   endfunction
 
+  " Make text bigger or smaller with a binding
   nnoremap <expr><C-=> ChangeScaleFactor(1.05)
   nnoremap <expr><C--> ChangeScaleFactor(1/1.05)
   nnoremap <expr><C-0> ResetScaleFactor()
+
+  " Easy navigation between splits
+  nnoremap <C-H> <C-W><C-H>
+  nnoremap <C-J> <C-W><C-J>
+  nnoremap <C-K> <C-W><C-K>
+  nnoremap <C-L> <C-W><C-L>
 endif
 ]], true)
 
