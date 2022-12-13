7d1299340e97d0c5ebbcc99ee7cf2b72d736be62
Alex Culliere <nachasic@gmail.com>
2022-12-13 15:57:44 +0200

    Add telescope

--- after/plugin/mappings.lua
+++ after/plugin/mappings.lua
@@ -1,27 +1,43 @@
 local wk = require("which-key")
+local tscp = require('telescope.builtin')
 
 wk.register({
-    e = {
-        name = "Explore",
-        -- x = { "<cmd>:Ex<CR>", "File explorer" }, -- Disabled in favor of NvimTree
-        t = { "<cmd>:NvimTreeToggle<CR>", "Toggle NvimTree" },
-        f = { "<cmd>:NvimTreeFocus<CR>", "Focus NvimTree" },
-        F = { "<cmd>:NvimTreeFindFile<CR>", "Focus current file" },
-        c = { "<cmd>:NvimTreeCollapse<CR>", "Collapse" },
+  e = {
+    name = "Explore",
+    -- x = { "<cmd>:Ex<CR>", "File explorer" }, -- Disabled in favor of NvimTree
+    t = { "<cmd>:NvimTreeToggle<CR>", "Toggle NvimTree" },
+    f = { "<cmd>:NvimTreeFocus<CR>", "Focus NvimTree" },
+    F = { "<cmd>:NvimTreeFindFile<CR>", "Focus current file" },
+    c = { "<cmd>:NvimTreeCollapse<CR>", "Collapse" },
+  },
+  l = {
+    name = "LSP",
+    e = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Diagnostics" },
+    k = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
+    s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
+    r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
+    g = {
+      name = "go to",
+      d = { tscp.lsp_definitions, "Definition" },
+      D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
+      i = { tscp.lsp_implementations, "Implementation" },
+      r = { tscp.lsp_references, "References" },
+      t = { tscp.lsp_type_definitions, "Type definitions" },
     },
-    l = {
-        name = "LSP",
-        e = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Diagnostics" },
-        k = { "<cmd>lua vim.lsp.buf.hover()<CR>", "Hover" },
-        s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" },
-        r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
-        g = {
-            name = "go to",
-            d = { "<cmd>lua vim.lsp.buf.definition()<CR>", "Definition" },
-            D = { "<cmd>lua vim.lsp.buf.declaration()<CR>", "Declaration" },
-            i = { "<cmd>lua vim.lsp.buf.implementation()<CR>", "Implementation" },
-            r = { "<cmd>lua vim.lsp.buf.references()<CR>", "References" },
-        },
-
+  },
+  f = {
+    name = "Find",
+    f = { tscp.find_files, "Files" },
+    s = { tscp.live_grep, "Grep string" },
+    b = { tscp.buffers, "Buffers" },
+    d = { tscp.diagnostics, "Diagnostics" },
+    g = {
+      name = "Git",
+      C = { tscp.git_commits, "Commits" },
+      c = { tscp.git_bcommits, "Buffer commits" },
+      b = { tscp.git_branches, "Branches" },
+      s = { tscp.git_status, "Current changes" },
+      S = { tscp.git_stash, "Stash" },
     }
+  }
 }, { prefix = "<leader>" })
